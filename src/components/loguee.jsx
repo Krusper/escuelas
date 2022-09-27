1c7dfb4b659caa1861a214076272962e204ecbaf
Krusper <krusper28@gmail.com>
2022-09-26 23:56:04 -0600

    Primeras configuracions
    Creacion de los primeros archivos necesarios para el proyecto

---
+++ src/components/loguee.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import login from '../themes/loguee.css'
+
+function Loguee() {
+    
+  return (
+    <div></div>
+  )
+}
+
+export default Loguee
+
---
+++ src/views/login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import login from '../themes/login.css'
+import Loguee from '../components/loguee'
+
+function Login(){
+  return (
+    <>
+    {
+        <div className='App'>
+            <div className='body'>
+                <section>
+                    <div></div>
+                    <div className='component-loguee'><Loguee></Loguee></div>
+                </section>
+            </div>
+        </div>
+    }
+    </>
+  )
+}
+
+export default Login
