e3edb6298f4aa82e47c396a15e2c88750adef997
Guadalupe Nayeli Leon Ochoa <57240149+Nayelileon@users.noreply.github.com>
2023-02-01 21:55:49 -0700

    Tabla cortemes

--- src/components/CorteMes.jsx
+++ src/components/CorteMes.jsx
@@ -1,25 +1,49 @@
 import React from 'react'
 import Header from './header'
-import { DataTable } from '@material-ui/core'
 import '../themes/CorteMes.css'
-
+import DataTable from 'react-data-table-component'
 
 function CorteMes() {
+  
+  const columns = [
+    {
+        name: 'ID',
+        selector: row => row.id,
+    },
+    {
+        name: 'Fecha',
+        selector: row => row.fecha,
+    },
+    {
+        name: 'Ingresos',
+        selector: row => row.ingreso,
+    },
+    {
+        name: 'Egresos',
+        selector: row => row.egreso,
+    },
+    {
+      name: 'Total',
+      selector: row => row.total,
+    },
+  ];  
 
   return (
 
-    <div className='corteMes'>
+    // <div className='corteMes'>
 
-      <div className='table'>
-        <div className='corteTabla'>
-          <p>RELACION DE CORTES MENSUALES</p>
-        </div>
-      </div>
+    //   <div className='table'>
+    //     <div className='corteTabla'>
+    //       <p>RELACION DE CORTES MENSUALES</p>
+    //     </div>
+    //   </div>
 
-    </div>
-
-
-  )
+    // </div>
+    <DataTable
+      columns={columns}
+      // data = {data}
+    />
+  );
 }
 
 export default CorteMes
